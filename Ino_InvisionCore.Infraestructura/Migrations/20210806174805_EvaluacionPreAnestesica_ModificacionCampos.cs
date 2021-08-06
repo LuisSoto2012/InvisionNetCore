aab5e6b7842ab0523f1f11e0a68e1eeca32d7e54
GianIno <giancarlo.zevallos.92@gmail.com>
2021-08-06 12:59:25 -0500

    citas web

---
+++ Ino_InvisionCore.Infraestructura/Migrations/20210806174805_EvaluacionPreAnestesica_ModificacionCampos.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ino_InvisionCore.Infraestructura.Migrations
+{
+    public partial class EvaluacionPreAnestesica_ModificacionCampos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.AlterColumn<string>(
+            //    name: "ExamenFisico_LaringoscopiaDificil",
+            //    table: "EvaluacionPreAnestesica",
+            //    nullable: true,
+            //    oldClrType: typeof(bool));
+
+            //migrationBuilder.AddColumn<string>(
+            //    name: "OjoProcedimiento",
+            //    table: "EvaluacionPreAnestesica",
+            //    nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DropColumn(
+            //    name: "OjoProcedimiento",
+            //    table: "EvaluacionPreAnestesica");
+
+            //migrationBuilder.AlterColumn<bool>(
+            //    name: "ExamenFisico_LaringoscopiaDificil",
+            //    table: "EvaluacionPreAnestesica",
+            //    nullable: false,
+            //    oldClrType: typeof(string),
+            //    oldNullable: true);
+        }
+    }
+}
