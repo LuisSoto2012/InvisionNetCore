44a79eb8064bb0e99d83ba1d2f49464bc8a4b83f
GianIno <giancarlo.zevallos.92@gmail.com>
2021-08-09 12:49:30 -0500

    ordenesMedicas

---
+++ Ino_InvisionCore.Infraestructura/Migrations/20210809174704_OrdenesMedicas_TiempoCita.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ino_InvisionCore.Infraestructura.Migrations
+{
+    public partial class OrdenesMedicas_TiempoCita : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TiempoCita",
+                table: "OrdenesMedicas",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TiempoCita",
+                table: "OrdenesMedicas");
+        }
+    }
+}
