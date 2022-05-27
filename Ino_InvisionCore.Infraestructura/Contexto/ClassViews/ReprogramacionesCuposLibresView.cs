9453e8d0568a717bb976795dd398816fd7c4e507
lsotofdev <luis.soto@pucp.pe>
2022-05-27 02:29:20 -0500

    atencion apis

--- Ino_InvisionCore.Infraestructura/Contexto/ClassViews/ReprogramacionesCuposLibresView.cs
+++ Ino_InvisionCore.Infraestructura/Contexto/ClassViews/ReprogramacionesCuposLibresView.cs
@@ -1,9 +1,17 @@
 // ReprogramacionesCuposLibresView.cs01:4301:43
 
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Ino_InvisionCore.Infraestructura.Contexto.ClassViews
 {
     public class ReprogramacionesCuposLibresView
     {
-        
+        [Key]
+        public Int64 RowId { get; set; }
+        public int IdProgramacion { get; set; }
+        public string HoraInicio { get; set; }
+        public string HoraFin { get; set; }
+        public string Cupo { get; set; }
     }
 }
